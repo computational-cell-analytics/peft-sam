a95b57db786b28db42e7f99a3aacb18583c8370e
Anwai Archit <anwai.archit@gmail.com>
2024-08-02 15:06:27 +0200

    Add peft_sam module

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='peft_sam',
+    version='0.0.1',
+    description='Parameter efficient finetuning methods for segmentation using SAM.',
+    author='Carolin Teuber, Anwai Archit',
+    author_email='carolin.teuber@stud.uni-goettingen.de, anwai.archit@uni-goettingen.de',
+    url='https://user.informatik.uni-goettingen.de/~pape41/',
+    packages=['peft_sam'],
+)
+
