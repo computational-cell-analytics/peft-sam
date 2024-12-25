b4645fee4729622d7bf0c4fbbd4abe7013fcfbad
Anwai Archit <anwai.archit@gmail.com>
2024-12-25 21:45:54 +0100

    Add preprocessing scripts for medical imaging datasets

---
+++ experiments/evaluation/preprocess_datasets.py
@@ -0,0 +1,180 @@
+import os
+from tqdm import tqdm
+
+import numpy as np
+
+from torch_em.data import datasets, MinInstanceSampler
+
+from tukra.io import write_image
+
+from micro_sam.training import identity
+
+
+def _transform_identity(raw, labels):  # This is done to avoid any transformations.
+    return raw, labels
+
+
+def _store_images(name, data_path, loader, view, is_rgb=False):
+    raw_dir = os.path.join(data_path, "slices", "raw")
+    labels_dir = os.path.join(data_path, "slices", "labels")
+    os.makedirs(raw_dir, exist_ok=True)
+    os.makedirs(labels_dir, exist_ok=True)
+
+    counter = 0
+    for x, y in tqdm(loader, desc=f"Preprocessing '{name}'"):
+        x, y = x.squeeze().numpy(), y.squeeze().numpy()
+
+        if is_rgb:  # Convert batch inputs to channels last.
+            x = x.transpose(1, 2, 0)
+
+        print(x.shape, y.shape)
+
+        if view:
+            import napari
+            v = napari.Viewer()
+            v.add_image(x, name="Image")
+            v.add_labels(y.astype("uint8"), name="Labels")
+            napari.run()
+
+        fname = f"image_{counter:05}.tif"
+        raw_path = os.path.join(raw_dir, fname)
+        labels_path = os.path.join(labels_dir, fname)
+
+        write_image(raw_path, x)
+        write_image(labels_path, y)
+
+        counter += 1
+
+
+def _process_papila_data(data_path, view):
+    loader = datasets.get_papila_loader(
+        path=os.path.join(data_path, "papila"),
+        batch_size=1,
+        patch_shape=(1, 512, 512),
+        split="test",
+        task="cup",
+        raw_transform=identity,
+        transform=_transform_identity,
+        sampler=MinInstanceSampler(),
+        resize_inputs=True,
+    )
+    _store_images("papila", os.path.join(data_path, "papila"), loader, view, is_rgb=True)
+
+
+def _process_motum_data(data_path, view):
+    loader = datasets.get_motum_loader(
+        path=os.path.join(data_path, "motum"),
+        batch_size=1,
+        patch_shape=(1, 512, 512),
+        split="test",
+        modality="flair",
+        raw_transform=identity,
+        transform=_transform_identity,
+        sampler=MinInstanceSampler(min_size=50),
+        resize_inputs=True,
+        n_samples=50,
+    )
+    _store_images("motum", os.path.join(data_path, "motum"), loader, view)
+
+
+def _process_psfhs_data(data_path, view):
+    loader = datasets.get_psfhs_loader(
+        path=os.path.join(data_path, "psfhs"),
+        batch_size=1,
+        patch_shape=(1, 512, 512),
+        split="test",
+        raw_transform=identity,
+        transform=_transform_identity,
+        sampler=MinInstanceSampler(),
+        resize_inputs=True,
+    )
+    _store_images("motum", os.path.join(data_path, "psfhs"), loader, view, is_rgb=True)
+
+
+def _process_jsrt_data(data_path, view):
+    def _label_trafo(label):  # maps labels to expected semantic structure.
+        neu_label = np.zeros_like(label)
+        neu_label[label == 255] = 1  # belongs to lungs
+        neu_label[label == 85] = 2  # belongs to heart
+        return neu_label
+
+    loader = datasets.get_jsrt_loader(
+        path=os.path.join(data_path, "jsrt"),
+        batch_size=1,
+        patch_shape=(512, 512),
+        split="test",
+        choice="Segmentation02",
+        raw_transform=identity,
+        transform=_transform_identity,
+        label_transform=_label_trafo,
+        sampler=MinInstanceSampler(),
+        resize_inputs=True,
+    )
+    _store_images("jsrt", os.path.join(data_path, "jsrt"), loader, view)
+
+
+def _process_amd_sd_data(data_path, view):
+    loader = datasets.get_amd_sd_loader(
+        path=os.path.join(data_path, "amd_sd"),
+        batch_size=1,
+        patch_shape=(1, 512, 512),
+        split="test",
+        raw_transform=identity,
+        transform=_transform_identity,
+        sampler=MinInstanceSampler(min_num_instances=6),  # this allows to get all labels.
+        resize_inputs=True,
+        n_samples=100,
+    )
+    loader.dataset.max_sampling_attempts = 10000
+    _store_images("amd-sd", os.path.join(data_path, "amd_sd"), loader, view, is_rgb=True)
+
+
+def _process_mice_tumseg_data(data_path, view):
+    def _raw_trafo(raw):
+        raw = raw.transpose(0, 2, 1)
+        return raw
+
+    def _label_trafo(label):
+        label = label.transpose(0, 2, 1)
+        return label
+
+    loader = datasets.get_mice_tumseg_loader(
+        path=os.path.join(data_path, "mice_tumseg"),
+        batch_size=1,
+        patch_shape=(1, 512, 512),
+        split="test",
+        raw_transform=_raw_trafo,
+        label_transform=_label_trafo,
+        transform=_transform_identity,
+        sampler=MinInstanceSampler(min_size=25),
+        resize_inputs=True,
+    )
+    _store_images("mice-tumorseg", os.path.join(data_path, "mice_tumseg"), loader, view)
+
+
+def main(args):
+    data_path = args.input_path
+    view = args.view
+
+    # Download the medical imaging datasets
+    # NOTE: uncomment the lines below to download datasets
+    from util import download_all_datasets
+    download_all_datasets(path=args.input_path, for_microscopy=False)
+
+    _process_papila_data(data_path, view)
+    _process_motum_data(data_path, view)
+    _process_psfhs_data(data_path, view)
+    _process_jsrt_data(data_path, view)
+    _process_amd_sd_data(data_path, view)
+    _process_mice_tumseg_data(data_path, view)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--input_path", type=str, default="/mnt/vast-nhr/projects/cidas/cca/experiments/peft_sam/data"
+    )
+    parser.add_argument("--view", action="store_true")
+    args = parser.parse_args()
+    main(args)
--- experiments/evaluation/util.py
+++ experiments/evaluation/util.py
@@ -8,27 +8,30 @@ from micro_sam.evaluation.livecell import _get_livecell_paths
 
 
 ROOT = "/scratch/usr/nimcarot/data/"
-
 EXPERIMENT_ROOT = "/scratch/projects/nim00007/sam/experiments/new_models"
 
 FILE_SPECS = {
     "platynereis/cilia": {"val": "platy_cilia_val_*", "test": "platy_cilia_test_*"},
 }
 
-# good spot to track all datasets we use atm
+# Good spot to track all datasets we use atm
 DATASETS = [
     # in-domain (LM)
     "livecell",
     # out-of-domain (LM)
     "covid_if", "orgasegment", "gonuclear",
     # organelles (EM)
-    #   - out-of-domain
-    "mitolab/glycolytic_muscle", "platynereis/cilia"
+    # - out-of-domain
+    "mitolab/glycolytic_muscle", "platynereis/cilia",
+    # TODO: other microscopy datasets are missing here I think.
+
+    # out-of-domain (MI)
+    "papila", "motum", "psfhs", "jsrt", "amd_sd", "mice_tumseg"
 ]
 
 
 def get_dataset_paths(dataset_name, split_choice):
-    # let's check if we have a particular naming logic to save the images
+    # Let's check if we have a particular naming logic to save the images
     try:
         file_search_specs = FILE_SPECS[dataset_name][split_choice]
         is_explicit_split = False
@@ -72,22 +75,52 @@ def get_pred_paths(prediction_folder):
     return pred_paths
 
 
-def download_all_datasets(path):
+def download_all_datasets(path, for_microscopy=True):
+    if for_microscopy:
+        # microscopy datasets
+        datasets.get_livecell_dataset(
+            path=os.path.join(path, "livecell"), split="test", patch_shape=(512, 512), download=True
+        )
+        datasets.get_platynereis_cilia_dataset(
+            os.path.join(path, "platynereis"), patch_shape=(1, 512, 512), download=True
+        )
+        print(
+            "MitoLab benchmark datasets need to downloaded separately. "
+            "See `torch_em.data.datasets.light_microscopy.cem.get_benchmark_datasets` for details."
+        )
+        datasets.get_covid_if_dataset(
+            path=os.path.join(path, "covid_if"), patch_shape=(1, 512, 512), download=True
+        )
+        datasets.get_orgasegment_dataset(
+            path=os.path.join(path, "orgasegment"), patch_shape=(1, 512, 512), download=True
+        )
+        datasets.get_gonuclear_dataset(
+            path=os.path.join(path, "gonuclear"), patch_shape=(1, 512, 512), download=True
+        )
+        datasets.get_hpa_segmentation_dataset(
+            path=os.path.join(path, "hpa"), patch_shape=(1, 512, 512), split="test", download=True
+        )
 
-    # platy-cilia
-    datasets.get_platynereis_cilia_dataset(os.path.join(path, "platynereis"), patch_shape=(1, 512, 512), download=True)
-
-    # mitolab
-    print("MitoLab benchmark datasets need to downloaded separately. See `datasets.cem.get_benchmark_datasets`")
-
-    # covid-if
-    datasets.get_covid_if_dataset(os.path.join(path, "covid_if"), patch_shape=(1, 512, 512), download=True)
-
-    # orgasegment
-    datasets.get_orgasegment_dataset(os.path.join(path, "orgasegment"), patch_shape=(1, 512, 512), download=True)
-
-    # gonuclear
-    datasets.get_gonuclear_dataset(os.path.join(path, "gonuclear"), patch_shape=(1, 512, 512), download=True)
+    else:
+        # medical imaging datasets
+        datasets.get_papila_dataset(
+            path=os.path.join(path, "papila"), patch_shape=(1, 512, 512), split="test", task="cup", download=True
+        )
+        datasets.get_motum_dataset(
+            path=os.path.join(path, "motum"), patch_shape=(1, 512, 512), split="test", modality="flair", download=True,
+        )
+        datasets.get_psfhs_dataset(
+            path=os.path.join(path, "psfhs"), patch_shape=(1, 512, 512), split="test", download=True,
+        )
+        datasets.get_jsrt_dataset(
+            path=os.path.join(path, "jsrt"), patch_shape=(1, 512, 512), split="test", download=True,
+        )
+        datasets.get_amd_sd_dataset(
+            path=os.path.join(path, "amd_sd"), patch_shape=(1, 512, 512), split="test", download=True,
+        )
+        datasets.get_mice_tumseg_dataset(
+            path=os.path.join(path, "mice_tumseg"), patch_shape=(1, 512, 512), split="test", download=True,
+        )
 
 #
 # PARSER FOR ALL THE REQUIRED ARGUMENTS
@@ -99,16 +132,30 @@ def get_default_arguments():
     parser.add_argument(
         "-m", "--model", type=str, required=True, help="Provide the model type to initialize the predictor"
     )
-    parser.add_argument("-c", "--checkpoint", type=none_or_str, default=None)
-    parser.add_argument("-e", "--experiment_folder", type=str, required=True)
-    parser.add_argument("-d", "--dataset", type=str, default=None)
-    parser.add_argument("--box", action="store_true", help="If passed, starts with first prompt as box")
+    parser.add_argument(
+        "-c", "--checkpoint", type=None, default=None, help="The filepath to the model checkpoint."
+    )
+    parser.add_argument(
+        "-e", "--experiment_folder", type=str, default="./experiments", help="The path to cache experiment files."
+    )
+    parser.add_argument(
+        "-d", "--dataset", type=str, required=True, help="The name of dataset."
+    )
+    parser.add_argument(
+        "--box", action="store_true", help="If passed, starts interactive segmentation with first prompt as box"
+    )
     parser.add_argument(
         "--use_masks", action="store_true", help="To use logits masks for iterative prompting."
     )
-    parser.add_argument("--peft_rank", default=None, type=int, help="The rank for peft method.")
-    parser.add_argument("--peft_module", default=None, type=str, help="The module for peft method. (e.g. LoRA or FacT)")
-    parser.add_argument("--dropout", default=None, type=float, help="The dropout factor for FacT finetuning")
+    parser.add_argument(
+        "--peft_rank", type=int, default=None, help="The rank for peft method."
+    )
+    parser.add_argument(
+        "--peft_module", default=None, type=str, help="The module for peft method. (e.g. LoRA or FacT)"
+    )
+    parser.add_argument(
+        "--dropout", default=None, type=float, help="The dropout factor for FacT finetuning"
+    )
     parser.add_argument(
         "--alpha", default=None, type=float, help="Scaling factor for peft method. (e.g. LoRA or AdaptFormer)"
     )
@@ -118,9 +165,3 @@ def get_default_arguments():
 
     args = parser.parse_args()
     return args
-
-
-def none_or_str(value):
-    if value == 'None':
-        return None
-    return value
