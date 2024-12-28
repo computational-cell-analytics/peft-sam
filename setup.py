cc69f9c398665febbbef9a94b2b36356f18a7f35
anwai98 <anwai.archit@gmail.com>
2024-12-28 17:08:31 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ from distutils.core import setup
 setup(
     name='peft_sam',
     version='0.0.1',
-    description='Parameter efficient finetuning methods for segmentation using SAM.',
-    author='Carolin Teuber, Anwai Archit',
-    author_email='carolin.teuber@stud.uni-goettingen.de, anwai.archit@uni-goettingen.de',
+    description='Parameter efficient finetuning methods for biomedical image segmentation using Segment Anything Models.',  # noqa
+    author='Carolin Teuber, Anwai Archit, Constantin Pape',
+    author_email='carolin.teuber@stud.uni-goettingen.de, anwai.archit@uni-goettingen.de, constantin.pape@informatik.uni-goettingen.de',  # noqa
     url='https://user.informatik.uni-goettingen.de/~pape41/',
     packages=['peft_sam'],
 )
-
