4e05c378606c5a1ab42e6faa4fc84ffd45c6c7f8
Anwai Archit <anwai.archit@gmail.com>
2025-01-09 22:37:41 +0100

    Minor update to legacy installation

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
+import runpy
 from distutils.core import setup
+
+
+__version__ = runpy.run_path("peft_sam/__version__.py")["__version__"]
+
 
 setup(
     name='peft_sam',
-    version='0.0.1',
-    description='Parameter efficient finetuning methods for segmentation using SAM.',
+    description='Parameter Efficient Fine-Tuning (PEFT) methods for Segment Anything Models.',
+    version=__version__,
     author='Carolin Teuber, Anwai Archit',
     author_email='carolin.teuber@stud.uni-goettingen.de, anwai.archit@uni-goettingen.de',
-    url='https://user.informatik.uni-goettingen.de/~pape41/',
+    url='https://github.com/computational-cell-analytics/PEFT_SAM',
     packages=['peft_sam'],
 )
-
