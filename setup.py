fc2a5f01e305040a24da74a2ea081d27c06cebfb
Anwai Archit <anwai.archit@gmail.com>
2025-10-08 17:25:23 +0200

    Update setup.py for find_packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import runpy
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 
 __version__ = runpy.run_path("peft_sam/__version__.py")["__version__"]
@@ -14,6 +14,6 @@ setup(
     author='Carolin Teuber, Anwai Archit, Constantin Pape',
     author_email='carolin.teuber@stud.uni-goettingen.de, anwai.archit@uni-goettingen.de, constantin.pape@informatik.uni-goettingen.de',
     url='https://github.com/computational-cell-analytics/peft-sam',
-    packages=['peft_sam'],
+    packages=find_packages(include=['peft_sam', 'peft_sam.*']),
     license="MIT",
 )
